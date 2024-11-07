81eb519ca5514d574b0ac921ee18fa3617e7f19e
Salman Khandakar <salman@pitangent.com>
2024-11-07 12:40:16 +0530

    modify project structure

--- app/apiRoute/v1/api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.apiRoute.v1.endpoints import auth, users, tenants
-# from ...apiRoute.v1.endpoints import auth, users, tenants
-
-api_router = APIRouter()
-
-api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
-api_router.include_router(users.router, prefix="/users", tags=["users"])
-api_router.include_router(tenants.router, prefix="/tenants", tags=["tenants"])
